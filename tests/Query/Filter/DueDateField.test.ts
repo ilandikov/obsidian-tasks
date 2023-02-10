3298ad2314a85c2bba4bca6c622f86a34ddbb553
Ilyas Landikov <ilyas.landikov@icloud.com>
2023-02-10 12:06:33 +0600

    refactor: unite tests in one

--- tests/Query/Filter/DueDateField.test.ts
+++ tests/Query/Filter/DueDateField.test.ts
@@ -63,30 +63,20 @@ describe('due date', () => {
             ['last', 'week', '2022-01-02', '2022-01-03', '2022-01-09', '2022-01-10'],
             ['this', 'week', '2022-01-09', '2022-01-10', '2022-01-16', '2022-01-17'],
             ['next', 'week', '2022-01-16', '2022-01-17', '2022-01-23', '2022-01-24'],
-        ])(
-            'due %s %s (After %s, between %s and %s, before %s)',
-            (indicator: string, range: string, dateBefore: string, dateBegin: string, dateEnd: string, dateAfter: string) => {
-                const filter = new DueDateField().createFilterOrErrorMessage(`due ${indicator} ${range}`);
 
-                // Test filter presence
-                expect(filter).toBeValid();
-
-                // Test filter function
-                testTaskFilterForTaskWithDueDate(filter, null, false);
-                testTaskFilterForTaskWithDueDate(filter, dateBefore, false);
-                testTaskFilterForTaskWithDueDate(filter, dateBegin, true);
-                testTaskFilterForTaskWithDueDate(filter, dateEnd, true);
-                testTaskFilterForTaskWithDueDate(filter, dateAfter, false);
-            },
-        );
-
-        it.each([
             ['last', 'month', '2021-11-30', '2021-12-01', '2021-12-31', '2022-01-01'],
             ['this', 'month', '2021-12-31', '2022-01-01', '2022-01-31', '2022-02-01'],
             ['next', 'month', '2021-01-31', '2022-02-01', '2022-02-28', '2022-03-01'],
         ])(
             'due %s %s (After %s, between %s and %s, before %s)',
-            (indicator: string, range: string, dateBefore: string, dateBegin: string, dateEnd: string, dateAfter: string) => {
+            (
+                indicator: string,
+                range: string,
+                dateBefore: string,
+                dateBegin: string,
+                dateEnd: string,
+                dateAfter: string,
+            ) => {
                 const filter = new DueDateField().createFilterOrErrorMessage(`due ${indicator} ${range}`);
 
                 // Test filter presence

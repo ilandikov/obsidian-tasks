f5abe7a161786de44b4651c9ff6ff2c5b63a0c6a
Ilyas Landikov <ilyas.landikov@icloud.com>
2026-07-10 19:06:42 +0600

    test: - display column title

--- tests/Renderer/ColumnRenderer.test.ts
+++ tests/Renderer/ColumnRenderer.test.ts
@@ -6,8 +6,10 @@ import type { QueryResult } from '../../src/Query/QueryResult';
 import { HtmlQueryResultsRenderer } from '../../src/Renderer/HtmlQueryResultsRenderer';
 import { createAndAppendElement } from '../../src/Renderer/TaskLineRenderer';
 import { TasksFile } from '../../src/Scripting/TasksFile';
+import { Priority } from '../../src/Task/Priority';
 import type { Task } from '../../src/Task/Task';
 import { mockApp } from '../__mocks__/obsidian';
+import { TaskBuilder } from '../TestingTools/TaskBuilder';
 import { makeHtmlQueryRendererParameters, mockHTMLRenderer, verifyRenderedTasks } from './RenderingTestHelpers';
 
 class ColumnRenderer {
@@ -19,9 +21,20 @@ class ColumnRenderer {
 
     async render(queryResult: QueryResult, container: HTMLDivElement) {
         const columnContainer = createAndAppendElement('div', container);
-        columnContainer.classList.add('tasks-column-container');
-
+        columnContainer.classList.add('tasks-columns');
         this.taskListRenderer.content = columnContainer;
+
+        const column = createAndAppendElement('div', columnContainer);
+        column.classList.add('tasks-column');
+
+        queryResult.taskGroups.groups.forEach((group) => {
+            group.groupHeadings.forEach((heading) => {
+                const title = createAndAppendElement('div', column);
+                title.classList.add('tasks-column-title');
+                title.textContent = heading.displayName;
+            });
+        });
+
         await this.taskListRenderer.renderQuery(State.Warm, queryResult);
     }
 }
@@ -53,6 +66,17 @@ describe('column rendering', () => {
     it('should render task list without columns', async () => {
         const source = '';
         const tasks: Task[] = [];
+        const queryResult = new Query(source).applyQueryToTasks(tasks);
+        const container = document.createElement('div');
+
+        await new ColumnRenderer(new FakeListRenderer(source, tasks)).render(queryResult, container);
+
+        verifyRenderedTasks(container, []);
+    });
+
+    it('should render task list with priority columns', async () => {
+        const source = 'view columns by priority';
+        const tasks: Task[] = [new TaskBuilder().priority(Priority.High).build()];
         const queryResult = new Query(source).applyQueryToTasks(tasks);
         const container = document.createElement('div');
 

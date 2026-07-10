8ccecd4db710c5aaf7cc8df795ac316e804030eb
Ilyas Landikov <ilyas.landikov@icloud.com>
2026-07-10 18:32:25 +0600

    test: - hook up column renderer with HtmlQueryResultsRenderer for later use

--- tests/Renderer/ColumnRenderer.test.ts
+++ tests/Renderer/ColumnRenderer.test.ts
@@ -1,21 +1,62 @@
+import { GlobalQuery } from '../../src/Config/GlobalQuery';
+import { State } from '../../src/Obsidian/Cache';
 import { Query } from '../../src/Query/Query';
+import { getQueryForQueryRenderer } from '../../src/Query/QueryRendererHelper';
 import type { QueryResult } from '../../src/Query/QueryResult';
+import { HtmlQueryResultsRenderer } from '../../src/Renderer/HtmlQueryResultsRenderer';
 import { createAndAppendElement } from '../../src/Renderer/TaskLineRenderer';
-import { verifyRenderedTasks } from './RenderingTestHelpers';
+import { TasksFile } from '../../src/Scripting/TasksFile';
+import type { Task } from '../../src/Task/Task';
+import { mockApp } from '../__mocks__/obsidian';
+import { makeHtmlQueryRendererParameters, mockHTMLRenderer, verifyRenderedTasks } from './RenderingTestHelpers';
 
 class ColumnRenderer {
-    render(_queryResult: QueryResult, container: HTMLDivElement) {
+    private readonly taskListRenderer: HtmlQueryResultsRenderer;
+
+    constructor(taskListRenderer: HtmlQueryResultsRenderer) {
+        this.taskListRenderer = taskListRenderer;
+    }
+
+    async render(queryResult: QueryResult, container: HTMLDivElement) {
         const columnContainer = createAndAppendElement('div', container);
         columnContainer.classList.add('tasks-column-container');
+
+        this.taskListRenderer.content = columnContainer;
+        await this.taskListRenderer.renderQuery(State.Warm, queryResult);
+    }
+}
+
+class FakeListRenderer extends HtmlQueryResultsRenderer {
+    constructor(source: string, allTasks: Task[]) {
+        const tasksFile = new TasksFile('stub-path.md');
+        const query = getQueryForQueryRenderer(source, GlobalQuery.getInstance(), tasksFile);
+
+        super(
+            () => Promise.resolve(),
+            null,
+            mockApp,
+            mockHTMLRenderer,
+            makeHtmlQueryRendererParameters(allTasks),
+            source,
+            tasksFile,
+            query,
+        );
+    }
+
+    public async renderQuery(_state: State, _queryResult: QueryResult): Promise<void> {
+        // @ts-expect-error inject fake HTML tag
+        createAndAppendElement('fake-task-list', this.content);
     }
 }
 
 describe('column rendering', () => {
-    it('should render task count if no tasks were found', () => {
-        const queryResult = new Query('').applyQueryToTasks([]);
+    it('should render task count if no tasks were found', async () => {
+        const source = '';
+        const tasks: Task[] = [];
+        const queryResult = new Query(source).applyQueryToTasks(tasks);
         const container = document.createElement('div');
 
-        new ColumnRenderer().render(queryResult, container);
+        await new ColumnRenderer(new FakeListRenderer(source, tasks)).render(queryResult, container);
 
         verifyRenderedTasks(container, []);
     });
